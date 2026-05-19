681685c7041de6e6dd66f920a6b03f5929d524aa
dineshvarunshankar <dineshvarunshankar@gmail.com>
2026-05-19 01:30:04 +0000

    simple custom env setup

---
+++ source/uav_indoor/uav_indoor/assets/starling_2/starling_2.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the quadcopters"""
+
+from __future__ import annotations
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets import ArticulationCfg
+#from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+##
+# Configuration
+##
+STARLING2_USD = (
+    "omniverse://airlab-nucleus.andrew.cmu.edu/Public/DTC/starling2_preliminary.usd"
+)
+
+STARLING2_CFG = ArticulationCfg(
+    prim_path="{ENV_REGEX_NS}/Robot",
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=STARLING2_USD,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=10.0,
+            enable_gyroscopic_forces=True,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
+        ),
+        copy_from_source=False,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.5), #spawn position of robot root
+        joint_pos={
+            ".*": 0.0,
+        },
+        #at every reset, the joints start at below velocities(rad/s)
+        joint_vel={
+            "joint0": 200.0,
+            "joint1": -200.0,
+            "joint2": 200.0,
+            "joint3": -200.0,
+        },
+    ),
+    actuators={
+        "dummy": ImplicitActuatorCfg( #add more actuators if required (for example, gimbal, gripper, etc.)
+            joint_names_expr=[".*"], #all joints
+            stiffness=0.0, # P gain for position control
+            damping=0.0, # D gain for velocity control
+        ),
+    },
+)
+"""Configuration for the ModalAI Starling 2 quadcopter."""
--- source/uav_indoor/uav_indoor/tasks/manager_based/uav_indoor/uav_indoor_env_cfg.py
+++ source/uav_indoor/uav_indoor/tasks/manager_based/uav_indoor/uav_indoor_env_cfg.py
@@ -20,20 +20,26 @@ from isaaclab.utils import configclass
 from . import mdp
 
 ##
-# Pre-defined configs
+# custom configs
 ##
 
-from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
+from uav_indoor.assets.starling_2.starling_2 import STARLING2_CFG
 
 
 ##
 # Scene definition
 ##
-
+scene_usd = "omniverse://airlab-nucleus.andrew.cmu.edu/Public/AirStack/scenes/urban/abandoned_city/After_the_rain...usdz"
 
 @configclass
 class UavIndoorSceneCfg(InteractiveSceneCfg):
     """Configuration for a cart-pole scene."""
+    # room
+    environment = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/Environment",
+        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0)),
+        spawn=sim_utils.UsdFileCfg(usd_path=scene_usd),
+    )
 
     # ground plane
     ground = AssetBaseCfg(
@@ -42,7 +48,7 @@ class UavIndoorSceneCfg(InteractiveSceneCfg):
     )
 
     # robot
-    robot: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+    robot: ArticulationCfg = STARLING2_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
 
     # lights
     dome_light = AssetBaseCfg(
@@ -60,7 +66,7 @@ class UavIndoorSceneCfg(InteractiveSceneCfg):
 class ActionsCfg:
     """Action specifications for the MDP."""
 
-    joint_effort = mdp.JointEffortActionCfg(asset_name="robot", joint_names=["slider_to_cart"], scale=100.0)
+    joint_effort = mdp.JointEffortActionCfg(asset_name="robot", joint_names=["joint0", "joint1", "joint2", "joint3"], scale=1.0)
 
 
 @configclass
@@ -71,9 +77,18 @@ class ObservationsCfg:
     class PolicyCfg(ObsGroup):
         """Observations for policy group."""
 
-        # observation terms (order preserved)
-        joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
+        # body
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel)
+        base_ang_vel = ObsTerm(func=mdp.base_ang_vel)
+        projected_gravity = ObsTerm(func=mdp.projected_gravity)
+        base_pos_z = ObsTerm(func=mdp.base_pos_z)
+
+        #rotors
+        #joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
         joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel)
+
+        #prev actions
+        last_action = ObsTerm(func=mdp.last_action)
 
         def __post_init__(self) -> None:
             self.enable_corruption = False
@@ -85,28 +100,53 @@ class ObservationsCfg:
 
 @configclass
 class EventCfg:
-    """Configuration for events."""
+    """Reset events."""
 
-    # reset
-    reset_cart_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
+    # Randomize drone pose (position/orientation/velocity) around defaults in starling_2.py
+    reset_base = EventTerm(
+        func=mdp.reset_root_state_uniform,
         mode="reset",
         params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]),
-            "position_range": (-1.0, 1.0),
-            "velocity_range": (-0.5, 0.5),
+            "asset_cfg": SceneEntityCfg("robot"),
+            "pose_range": {
+                "x": (-0.5, 0.5),
+                "y": (-0.5, 0.5),
+                "z": (0.0, 0.3),          # extra height on top of init_state.pos z
+                "roll": (-0.1, 0.1),
+                "pitch": (-0.1, 0.1),
+                "yaw": (-3.14, 3.14),
+            },
+            "velocity_range": {
+                "x": (0.0, 0.0),
+                "y": (0.0, 0.0),
+                "z": (0.0, 0.0),
+                "roll": (0.0, 0.0),
+                "pitch": (0.0, 0.0),
+                "yaw": (0.0, 0.0),
+            },
         },
     )
 
-    reset_pole_position = EventTerm(
+    # Props: no random angle/spin at reset
+    reset_rotor_joints = EventTerm(
         func=mdp.reset_joints_by_offset,
         mode="reset",
         params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
-            "position_range": (-0.25 * math.pi, 0.25 * math.pi),
-            "velocity_range": (-0.25 * math.pi, 0.25 * math.pi),
+            "asset_cfg": SceneEntityCfg("robot", joint_names=["joint0", "joint1", "joint2", "joint3"]),
+            "position_range": (0.0, 0.0),
+            "velocity_range": (0.0, 0.0),
         },
     )
+
+    # reset_pole_position = EventTerm(
+    #     func=mdp.reset_joints_by_offset,
+    #     mode="reset",
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
+    #         "position_range": (-0.25 * math.pi, 0.25 * math.pi),
+    #         "velocity_range": (-0.25 * math.pi, 0.25 * math.pi),
+    #     },
+    # )
 
 
 @configclass
@@ -116,25 +156,50 @@ class RewardsCfg:
     # (1) Constant running reward
     alive = RewTerm(func=mdp.is_alive, weight=1.0)
     # (2) Failure penalty
-    terminating = RewTerm(func=mdp.is_terminated, weight=-2.0)
+    terminating = RewTerm(func=mdp.is_terminated, weight=-10.0)
     # (3) Primary task: keep pole upright
-    pole_pos = RewTerm(
-        func=mdp.joint_pos_target_l2,
-        weight=-1.0,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]), "target": 0.0},
+    # pole_pos = RewTerm(
+    #     func=mdp.joint_pos_target_l2,
+    #     weight=-1.0,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]), "target": 0.0},
+    # )
+    # # (4) Shaping tasks: lower cart velocity
+    # cart_vel = RewTerm(
+    #     func=mdp.joint_vel_l1,
+    #     weight=-0.01,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
+    # )
+    # # (5) Shaping tasks: lower pole angular velocity
+    # pole_vel = RewTerm(
+    #     func=mdp.joint_vel_l1,
+    #     weight=-0.005,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
+    # )
+    # hover near target height (world z; tune to match spawn ~0.5–1.0 m)
+    height = RewTerm(
+        func=mdp.base_height_l2,
+        weight=-2.0,
+        params={"target_height": 1.0, "asset_cfg": SceneEntityCfg("robot")},
     )
-    # (4) Shaping tasks: lower cart velocity
-    cart_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.01,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
+    # penalize tilt (upright)
+    flat_orientation = RewTerm(
+        func=mdp.flat_orientation_l2,
+        weight=-0.5,
+        params={"asset_cfg": SceneEntityCfg("robot")},
     )
-    # (5) Shaping tasks: lower pole angular velocity
-    pole_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.005,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
+    # penalize vertical bobbing and wobble
+    lin_vel_z = RewTerm(
+        func=mdp.lin_vel_z_l2,
+        weight=-0.5,
+        params={"asset_cfg": SceneEntityCfg("robot")},
     )
+    ang_vel_xy = RewTerm(
+        func=mdp.ang_vel_xy_l2,
+        weight=-0.05,
+        params={"asset_cfg": SceneEntityCfg("robot")},
+    )
+    # smooth control
+    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-0.01)
 
 
 @configclass
@@ -143,10 +208,20 @@ class TerminationsCfg:
 
     # (1) Time out
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
-    # (2) Cart out of bounds
-    cart_out_of_bounds = DoneTerm(
-        func=mdp.joint_pos_out_of_manual_limit,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]), "bounds": (-3.0, 3.0)},
+    # # (2) Cart out of bounds
+    # cart_out_of_bounds = DoneTerm(
+    #     func=mdp.joint_pos_out_of_manual_limit,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]), "bounds": (-3.0, 3.0)},
+    # )
+    # hit ground / too low (tune minimum_height for your spawn height)
+    low_height = DoneTerm(
+        func=mdp.root_height_below_minimum,
+        params={"minimum_height": 0.15, "asset_cfg": SceneEntityCfg("robot")},
+    )
+    # flipped / too tilted (~57° if limit_angle=1.0 rad)
+    bad_orientation = DoneTerm(
+        func=mdp.bad_orientation,
+        params={"limit_angle": 1.0, "asset_cfg": SceneEntityCfg("robot")},
     )
 
 
@@ -158,7 +233,7 @@ class TerminationsCfg:
 @configclass
 class UavIndoorEnvCfg(ManagerBasedRLEnvCfg):
     # Scene settings
-    scene: UavIndoorSceneCfg = UavIndoorSceneCfg(num_envs=4096, env_spacing=4.0)
+    scene: UavIndoorSceneCfg = UavIndoorSceneCfg(num_envs=64, env_spacing=6.0, filter_collisions=True)
     # Basic settings
     observations: ObservationsCfg = ObservationsCfg()
     actions: ActionsCfg = ActionsCfg()
@@ -171,10 +246,10 @@ class UavIndoorEnvCfg(ManagerBasedRLEnvCfg):
     def __post_init__(self) -> None:
         """Post initialization."""
         # general settings
-        self.decimation = 2
-        self.episode_length_s = 5
+        self.decimation = 2 #how many physics sub-steps per simulation step
+        self.episode_length_s = 20
         # viewer settings
         self.viewer.eye = (8.0, 0.0, 5.0)
         # simulation settings
-        self.sim.dt = 1 / 120
+        self.sim.dt = 1 / 120 #simulation time step; with decimation as 2, the actual time step is 1/120 * 2 = 1/60 ~60Hz
         self.sim.render_interval = self.decimation
